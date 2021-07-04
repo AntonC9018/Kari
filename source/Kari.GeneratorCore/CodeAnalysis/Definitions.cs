b8e025c984faa513ab0c314a43a03b47dcfb5315
AntonC9018 <anton.curm@gmail.com>
2021-07-04 18:52:41 +0300

    Code generation seems to be working alright?

---
+++ source/Kari.GeneratorCore/CodeAnalysis/Definitions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#pragma warning disable SA1649 // File name should match first type name
+
+namespace Kari.GeneratorCore.CodeAnalysis
+{
+    public class GeneralInfo
+    {
+        public string Namespace { get; set; }
+
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
