9d12bfd2ec2520e6192fe3ffa3f119421d6016cd
Anton Curmanschii <anton.curm@gmail.com>
2022-03-27 17:46:00 +0300

    stuff

--- source/Kari.GeneratorCore/Workflow/AdministratorFinder.cs
+++ source/Kari.GeneratorCore/Workflow/AdministratorFinder.cs
@@ -4,19 +4,20 @@ using System.Reflection;
 
 namespace Kari.GeneratorCore.Workflow
 {
-    public static class AdministratorFinder
+    // Just a helper thing to load plugins, probably exists in this form just temporarily
+    public class AdministratorFinder
     {
-        public static readonly List<Assembly> Plugins = new List<Assembly>();
+        private List<Assembly> _plugins = new();
 
-        public static void LoadPlugin(string path)
+        public void LoadPlugin(string path)
         {
             var dll = Assembly.LoadFile(path);
-            Plugins.Add(dll);
+            _plugins.Add(dll);
         }
 
-        private static IEnumerable<System.Type> GetAdministratorTypes()
+        private IEnumerable<System.Type> GetAdministratorTypes()
         {
-            return Plugins.SelectMany(dll => dll.GetExportedTypes())
+            return _plugins.SelectMany(dll => dll.GetExportedTypes())
                 .Where(type => typeof(IAdministrator).IsAssignableFrom(type) && !type.IsAbstract);
         }
 
@@ -24,7 +25,7 @@ namespace Kari.GeneratorCore.Workflow
         /// Adds the administrators specified by name in `namesToAdd`, removing these names from there.
         /// The names must be in the correct case (exactly match the class names).
         /// </summary>
-        public static void AddAdministrators(MasterEnvironment environment, HashSet<string> namesToAdd)
+        public void AddAdministrators(MasterEnvironment environment, HashSet<string> namesToAdd)
         {
             foreach (var adminType in GetAdministratorTypes())
             {
@@ -36,7 +37,7 @@ namespace Kari.GeneratorCore.Workflow
             }
         }
 
-        public static void AddAllAdministrators(MasterEnvironment environment)
+        public void AddAllAdministrators(MasterEnvironment environment)
         {
             foreach (var adminType in GetAdministratorTypes())
             {
